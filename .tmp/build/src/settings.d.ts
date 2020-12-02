1284d30594a97c945e16452f35dd5da835653a94
merlin2002 <merlin2002@vip.qq.com>
2020-12-02 17:39:26 +0800

    20201202

---
+++ .tmp/build/src/settings.d.ts
@@ -0,0 +1,14 @@
+import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
+import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
+export declare class VisualSettings extends DataViewObjectsParser {
+    myproperties: myPropertiesSettings;
+}
+export declare class myPropertiesSettings {
+    defaultColor: string;
+    showAllDataPoints: boolean;
+    fill: string;
+    fillRule: string;
+    fontSize: number;
+    theme: string;
+    getthemecolor(themename: string): string;
+}
